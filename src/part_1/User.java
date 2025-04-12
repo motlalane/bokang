d7672dd1752997124102a5347b068cc8f3983258
Motlalane <ramolla@outlook.com>
2025-04-12 05:05:03 +0200

    Student login fixed

--- src/part_1/User.java
+++ src/part_1/User.java
@@ -10,29 +10,35 @@ package part_1;
  */
   public class User {
       private static User instance;
-      private String username;
-      private String password;
-      
-      public User() {
-          
-      }
+    private String username;
+    private String password;
+     private String firstName;
 
-    public static User getInstance() {
-        if (instance == null){
-            instance = new User();
-        }
-          return instance;
+    public User() {
     }
     
-
-    public void checkCredentials(String username, String password) {
-        this.username = username;
-          this.password = password;
+      public static User getInstance() {
+        if (instance == null) {
+            instance = new User();
+        }
+        return instance;
     }
 
-    void setCredentials(String username, String password) {
+    public void setCredentials(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public boolean checkCredentials(String username, String password) {
+        return this.username != null && this.username.equals(username) && this.password != null && this.password.equals(password);
+    }
+    
+      public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
     }
           
         
